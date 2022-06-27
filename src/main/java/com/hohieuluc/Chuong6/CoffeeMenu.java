7cd277e1668356493b9da29667e1b0f4b0fe5cb7
Ho Hieu Luc <kingofika123@gmail.com>
2022-06-27 12:43:12 +0700

    add some cool methods

--- src/main/java/com/hohieuluc/Chuong6/CoffeeMenu.java
+++ src/main/java/com/hohieuluc/Chuong6/CoffeeMenu.java
@@ -7,6 +7,10 @@ import java.util.Scanner;
 public class CoffeeMenu {
     List<Coffee> coffeeMenu;
 
+    public List<Coffee> getCoffeeMenu() {
+        return this.coffeeMenu;
+    }
+
     public CoffeeMenu() {
         this.coffeeMenu = new ArrayList<Coffee>() {
             {
@@ -29,22 +33,44 @@ public class CoffeeMenu {
         System.out.println("Hoa don: " + coffee.loaiCoffee + ", gia: " + coffee.gia * soLuong);
     }
 
-    public static void main(String[] args) {
-        CoffeeMenu coffeeMenu = new CoffeeMenu();
-        coffeeMenu.inMenu();
-
-        Scanner scanner = new Scanner(System.in);
+    public int chonSoThuTuCoffee(Scanner scanner) {
         int soThuTuCoffee;
         do {
             System.out.println("Chon loai coffee: ");
             soThuTuCoffee = scanner.nextInt();
-        } while (soThuTuCoffee < 1 ||  soThuTuCoffee > coffeeMenu.coffeeMenu.size());
+        } while (soThuTuCoffee < 1 || soThuTuCoffee > coffeeMenu.size());
 
-        System.out.println("Chon so luong: ");
-        int soLuong = scanner.nextInt();
+        return soThuTuCoffee;
+    }
 
-        coffeeMenu.inHoaDon(coffeeMenu.coffeeMenu.get(soThuTuCoffee - 1), soLuong);
+    public int chonSoLuong(Scanner scanner) {
+        int soLuong;
+        do {
+            System.out.println("Chon so luong: ");
+            soLuong = scanner.nextInt();
+        } while (soLuong < 1);
 
+        return soLuong;
+    }
+
+    public void addCoffee(Coffee coffee) {
+        this.coffeeMenu.add(coffee);
+    }
+
+    public Coffee getCoffeeBySoThuTu (int soThuTuCoffee) {
+        return this.coffeeMenu.get(soThuTuCoffee - 1);
+    }
+
+    public static void main(String[] args) {
+        CoffeeMenu coffeeMenu = new CoffeeMenu();
+        coffeeMenu.inMenu();
+        Scanner scanner = new Scanner(System.in);
+
+        int soThuTuCoffee = coffeeMenu.chonSoThuTuCoffee(scanner);
+        int soLuong = coffeeMenu.chonSoLuong(scanner);
+        
         scanner.close();
+
+        coffeeMenu.inHoaDon(coffeeMenu.getCoffeeBySoThuTu(soThuTuCoffee), soLuong);
     }
 }
--- src/main/java/com/hohieuluc/Chuong6/SCoffeeMenu.java
+++ src/main/java/com/hohieuluc/Chuong6/SCoffeeMenu.java
@@ -2,42 +2,36 @@ package com.hohieuluc.Chuong6;
 
 import java.util.Scanner;
 
-public class SCoffeeMenu extends CoffeeMenu{
-    public Coffee chonCoffee(int soThuTuCoffee) {
+public class SCoffeeMenu {
+    public Coffee getCoffeeBySoThuTu(CoffeeMenu coffeeMenu, int soThuTuCoffee) {
         switch (soThuTuCoffee) {
             case 1:
-                return coffeeMenu.get(0);
+                return coffeeMenu.getCoffeeMenu().get(0);
             case 2:
-                return coffeeMenu.get(1);
+                return coffeeMenu.getCoffeeMenu().get(1);
             case 3:
-                return coffeeMenu.get(2);
+                return coffeeMenu.getCoffeeMenu().get(2);
             case 4:
-                return coffeeMenu.get(3);
+                return coffeeMenu.getCoffeeMenu().get(3);
             case 5:
-                return coffeeMenu.get(4);
+                return coffeeMenu.getCoffeeMenu().get(4);
             default:
                 return null;
         }
     }
-
 
     public static void main(String[] args) {
         CoffeeMenu coffeeMenu = new CoffeeMenu();
         coffeeMenu.inMenu();
 
         Scanner scanner = new Scanner(System.in);
-        int soThuTuCoffee;
-        do {
-            System.out.println("Chon loai coffee: ");
-            soThuTuCoffee = scanner.nextInt();
-        } while (soThuTuCoffee < 1 ||  soThuTuCoffee > coffeeMenu.coffeeMenu.size());
+        int soThuTuCoffee = coffeeMenu.chonSoThuTuCoffee(scanner);
+        int soLuong = coffeeMenu.chonSoLuong(scanner);
+        scanner.close();
 
         SCoffeeMenu scoffeeMenu = new SCoffeeMenu();
-        Coffee coffee = scoffeeMenu.chonCoffee(soThuTuCoffee);
-        System.out.println("Chon so luong: ");
-        int soLuong = scanner.nextInt();
-        coffeeMenu.inHoaDon(coffee, soLuong);
+        Coffee coffee = scoffeeMenu.getCoffeeBySoThuTu(coffeeMenu, soThuTuCoffee);
 
-        scanner.close();
+        coffeeMenu.inHoaDon(coffee, soLuong);
     }
 }
