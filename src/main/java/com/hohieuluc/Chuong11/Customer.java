4cd20272375bed8801982ed4020d2074def37175
Ho Hieu Luc <kingofika123@gmail.com>
2022-06-28 09:39:16 +0700

    add Chuong11

---
+++ src/main/java/com/hohieuluc/Chuong11/Customer.java
@@ -0,0 +1,7 @@
+package com.hohieuluc.Chuong11;
+
+public class Customer {
+    public static void main(String[] args) {
+        System.out.println("customer");
+    }
+}
---
+++ src/main/java/com/hohieuluc/Chuong11/Order.java
@@ -0,0 +1,7 @@
+package com.hohieuluc.Chuong11;
+
+public class Order {
+    public static void main(String[] args) {
+        System.out.println("order");
+    }
+}
