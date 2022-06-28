223361c1e8e9a98a562723060551d21d83e36b83
Ho Hieu Luc <kingofika123@gmail.com>
2022-06-28 09:38:19 +0700

    fix typo

--- src/main/java/com/hohieuluc/Chuong2/HoHieuLuc.java
+++ src/main/java/com/hohieuluc/Chuong2/HoHieuLuc.java
@@ -2,6 +2,6 @@ package com.hohieuluc.Chuong2;
 
 public class HoHieuLuc {
     public static void main(String[] args) {
-        System.out.println("Quyen Tam Thanh Cong!");
+        System.out.println("Quyet Tam Thanh Cong!");
     }
 }
